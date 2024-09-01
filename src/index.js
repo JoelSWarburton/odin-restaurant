b79ceff8a37ade5a89f562b1de1c0b25bf3062f6
Joel Warburton <Joel.s.warburton@gmail.com>
2024-09-02 00:19:29 +1000

    Add ids for buttons and add event listeners to fire page load events

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-console.log("Hello, world");
+
 import "./style.css";
+import { homePage } from "./home";
+import { aboutPage } from "./about";
+import { menuPage } from "./menu";
+
+
+
+document.querySelector("#home-btn").addEventListener("click", () => homePage());
+document.querySelector("#about-btn").addEventListener("click", () => aboutPage());
+document.querySelector("#menu-btn").addEventListener("click", () => menuPage());
+
+homePage();
