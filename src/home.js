a13fbfad5acc5268da47f94446a449b679046fc8
Joel Warburton <Joel.s.warburton@gmail.com>
2024-09-04 19:49:19 +1000

    Implement basic html structure for home page

--- src/home.js
+++ src/home.js
@@ -1,5 +1,25 @@
 function homePage() {
-    console.log("homepage loaded...");
+
+    let content = document.querySelector("#content");
+
+    let title = document.createElement("h1");
+    title.id = "page-title";
+    title.textContent = "Papa Joel's Pizza"
+
+    let subtitle = document.createElement("p");
+    subtitle.id = "subtitle";
+    subtitle.textContent = "The best pizza in the world!";
+
+    let titleCard = document.createElement("div");
+    titleCard.id = "title-card";
+
+
+    content.append(titleCard);
+    titleCard.append(title, subtitle);
+    
+
+
+
 
 }
 
