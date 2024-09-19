10ecc0e3ea38f9085a8138b0edf3191c4bc7ce56
Joel Warburton <Joel.s.warburton@gmail.com>
2024-09-19 12:47:24 +1000

    Track logo for pizza shop

--- src/home.js
+++ src/home.js
@@ -1,26 +1,24 @@
+import pizzaLogo from "./pizza-logo.png";
+
 function homePage() {
+  let content = document.querySelector("#content");
 
-    let content = document.querySelector("#content");
+  let title = document.createElement("h1");
+  title.id = "page-title";
+  title.textContent = "Papa Joel's Pizza";
 
-    let title = document.createElement("h1");
-    title.id = "page-title";
-    title.textContent = "Papa Joel's Pizza"
+  let logo = document.createElement("img");
+  logo.src = pizzaLogo;
 
-    let subtitle = document.createElement("p");
-    subtitle.id = "subtitle";
-    subtitle.textContent = "The best pizza in the world!";
+  let subtitle = document.createElement("p");
+  subtitle.id = "subtitle";
+  subtitle.textContent = "The best pizza in the world!";
 
-    let titleCard = document.createElement("div");
-    titleCard.id = "title-card";
+  let titleCard = document.createElement("div");
+  titleCard.id = "title-card";
 
-
-    content.append(titleCard);
-    titleCard.append(title, subtitle);
-    
-
-
-
-
+  content.append(titleCard);
+  titleCard.append(title, logo, subtitle);
 }
 
-export {homePage}
+export { homePage };
