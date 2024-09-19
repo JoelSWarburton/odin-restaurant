c6db5e3936f295acb3a53c794c248fe269c6642e
Joel Warburton <Joel.s.warburton@gmail.com>
2024-09-19 10:59:52 +1000

    Fix

--- src/home.js
+++ src/home.js
@@ -1,26 +1,3 @@
-function homePage() {
+function homePage() {}
 
-    let content = document.querySelector("#content");
-
-    let title = document.createElement("h1");
-    title.id = "page-title";
-    title.textContent = "Papa Joel's Pizza"
-
-    let subtitle = document.createElement("p");
-    subtitle.id = "subtitle";
-    subtitle.textContent = "The best pizza in the world!";
-
-    let titleCard = document.createElement("div");
-    titleCard.id = "title-card";
-
-
-    content.append(titleCard);
-    titleCard.append(title, subtitle);
-    
-
-
-
-
-}
-
-export {homePage}
+export { homePage };
